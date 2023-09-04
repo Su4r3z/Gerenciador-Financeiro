a0895fb5870084d1fd4adfa295bcb2851cf803cc
Su4r3z <vitoriosuarez.06@gmail.com>
2023-09-04 11:25:49 -0300

    atualizacao backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,31 @@
 const express = require("express");
+const mysql = require("mysql2/promise");
+
 const port = 3000
 const app = express();
 
+const conection = mysql.createPool({
+    host: 'localhost',
+    port: "3306",
+    database: "testePessoa",
+    user:"root",
+    password: ""
+})
 
-app.get('/', (req, res) => {
-    res.send("Ta tudo funcionando")
+app.use(express.json());
+
+app.get('/pessoa', async (req, res) => {
+    const consulta = await getAllPeople()
+    return res.status(200).json(consulta)
 });
+
+const getAllPeople = async () => {
+    const [query] = await conection.execute('select * from pessoa')
+    return query
+};
+
+
 
 
 app.listen(port,() => console.log(`Estou te ouvindo pela porta ${port}`));
+
